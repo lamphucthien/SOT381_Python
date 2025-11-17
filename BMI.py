46ff9ce75fee0afdedf60a08a02ec65f782bda9b
Lâm Phúc Thiện <lamphucthien36@gmail.com>
2025-11-17 10:03:15 +0700

    TinhBMI

---
+++ BMI.py
@@ -0,0 +1,5 @@
+can_nang = float(input("Hãy nhập cân nặng của bạn (Kg): "))
+chieu_cao = float(input("Hãy nhập chiều cao của bạn (m): "))
+cong_thuc = can_nang / (chieu_cao ** 2)
+
+print("Chỉ số BMI của bạn là: ", cong_thuc),
