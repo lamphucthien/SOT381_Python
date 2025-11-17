43b5617e500454a64033a967c2357fc6b9f3b4c3
Lâm Phúc Thiện <lamphucthien36@gmail.com>
2025-11-17 09:08:09 +0700

    Commit moi

--- C_to_F.py
+++ C_to_F.py
@@ -1,12 +1,12 @@
 def c_to_f(c):
     return c * 9/5 + 32
 while True:
- inp = input("Hãy nhập độ C, hoặc ấn f để dừng: ")
- if inp.lower() == 'f':
+ inf = input("Hãy nhập độ C, hoặc ấn f để dừng: ")
+ if inf.lower() == 'f':
      print("Kết thúc")
      break
  try:
-     C = float(inp)
+     C = float(inf)
      print("F= ", c_to_f(C))
  except ValueError:
      print("Vui lòng nhập số hợp lệ: ")
---
+++ Luyentap.py
@@ -0,0 +1,10 @@
+n = int(input())
+dem = 0
+i = 1
+
+while i <= n:
+
+    dem += 1
+    i += 1
+
+print(dem)
