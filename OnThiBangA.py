5c5e2ef2e192acffab5d3920f39d34406cb185bb
Lâm Phúc Thiện <lamphucthien36@gmail.com>
2025-11-18 16:57:08 +0700

    Update OnThiBangA.py

--- OnThiBangA.py
+++ OnThiBangA.py
@@ -4,19 +4,19 @@ dem = 0
 t = 1
 i = 2
 
-while t <= n: # Tính tổng các số từ 1 đến n chia hết cho 3 hoặc 5
+while t <= n:             # Tính tổng các số từ 1 đến n chia hết cho 3 hoặc 5
     if t % 3 == 0 or t % 5 == 0:
         tong += t
     t += 1
 print(tong)
 
-while i <= n : # Đếm số lượng số nguyên tố từ 2 đến n
-    kiem_tra = True
+while i <= n :            # Đếm số lượng số nguyên tố từ 2 đến n
+    kiem_tra = True  #Bắt đầu kiểm tra số nguyên tố
     j = 2
 
     while j < i:
-        if i % j == 0:
-            kiem_tra = False
+        if i % j == 0:  # TH không phải là số nguyên tố
+            kiem_tra = False 
             break
         j += 1
     if kiem_tra:
@@ -24,7 +24,7 @@ while i <= n : # Đếm số lượng số nguyên tố từ 2 đến n
     i += 1
 print(dem)
 
-if n <= 1: # Kiểm tra n có phải số nguyên tố hay không
+if n <= 1:                 #Kiểm tra n có phải số nguyên tố hay không
     print("NO")
 else:
     kiem_tra = True
@@ -39,3 +39,20 @@ else:
         print("YES")
     else:
         print("NO")
+
+tich = 1              # Tính giai thừa
+k = 1
+while k <= n:
+    tich *= i
+    k += 1
+    
+max_val = 0     # min_val = n + 1      # Nhập số nguyên n. Tìm số lớn nhất từ 1 → n chia hết cho 2 và 3 nhưng không chia hết cho 5. Nếu không có số nào thì in 0.
+l = 1
+while i <= n:
+    if i % 2 == 0 and i % 3 == 0 and i % 5 != 0:
+        if i > max_val: # i < min_val
+            max_val = i # min_val = i
+
+    i += 1
+
+print(max_val) # print(min_val)
