ced020377da2b66fcf722b8d0ffc99c8364e6e7c
Lâm Phúc Thiện <lamphucthien36@gmail.com>
2025-11-24 12:20:35 +0700

    Update

--- C_to_F.py
+++ C_to_F.py
@@ -1,12 +1,16 @@
 def c_to_f(c):
     return c * 9/5 + 32
+def c_to_k(c):
+    return c + 273.15
 while True:
  inf = input("Hãy nhập độ C, hoặc ấn f để dừng: ")
  if inf.lower() == 'f':
      print("Kết thúc")
      break
  try:
-     C = float(inf)
-     print("F= ", c_to_f(C))
+    C = float(inf)
+    print("F= ", c_to_f(C))
+    K = float(inf)
+    print("K= ", c_to_k(K))
  except ValueError:
      print("Vui lòng nhập số hợp lệ: ")
