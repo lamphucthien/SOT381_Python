b8f22678eb6bff42c49b5fa5e4e113937494f28b
Lâm Phúc Thiện <lamphucthien36@gmail.com>
2025-11-24 12:30:45 +0700

    TinhBMI

--- C_to_F&K.py
+++ C_to_F&K.py
@@ -1,12 +1,15 @@
 def c_to_f(c):
     return c * 9/5 + 32
+def c_to_k(c):
+    return c + 273.15
 while True:
  inf = input("Hãy nhập độ C, hoặc ấn f để dừng: ")
  if inf.lower() == 'f':
      print("Kết thúc")
      break
  try:
-     C = float(inf)
-     print("F= ", c_to_f(C))
+    C = float(inf)
+    print("F= ", c_to_f(C))
+    print("K= ", c_to_k(C))
  except ValueError:
      print("Vui lòng nhập số hợp lệ: ")
