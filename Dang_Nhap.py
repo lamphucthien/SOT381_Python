62226811c73f579add7589cb3674b43317a04258
Lâm Phúc Thiện <lamphucthien36@gmail.com>
2025-12-15 16:51:16 +0700

    Update Dang_Nhap.py

--- Dang_Nhap.py
+++ Dang_Nhap.py
@@ -1,7 +1,7 @@
 m = 3 #Giới hạn nhập
 n = 0 #Số lần nhập sai
 while True:
-    r = m - n
+    r = m - n #Số lần nhập còn lại
     inf = input(f"Hãy nhập Y hoặc y, bạn còn {r} lần nhập: ")
     if inf.lower() == 'y':
         print("Login success!")
