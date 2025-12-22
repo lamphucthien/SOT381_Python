36df19c2b95eda3dcad0662117e446df3c187c70
Lâm Phúc Thiện <lamphucthien36@gmail.com>
2025-12-22 14:16:38 +0700

    Format output for triangle perimeter and area

--- TH2212_Them_Bai1.py
+++ TH2212_Them_Bai1.py
@@ -1,4 +1,4 @@
 a, b, c = map(int, input().split())
 chu_vi = a + b + c
 dien_tich = (chu_vi*(chu_vi - a)*(chu_vi - b)*(chu_vi - c))**0,5
-print(chu_vi, dien_tich)
+print(f"Chu vi tam giác là {chu_vi},Diện tích tam giác là {dien_tich}")
